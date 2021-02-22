455de3e967c438352df541cdb3ba5859a58af84e
Pablo Alvarez <pablo.alvarez@ucb.edu.bo>
2021-02-22 10:37:13 -0400

    Initial commit

---
+++ TRABAJO1_DRAF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TRABAJO1_DRAF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
